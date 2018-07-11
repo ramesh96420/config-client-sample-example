52503bd60b27b300742504598f5c62c44e31757f
ramesh96420 <ramesh96420@gmail.com>
2018-07-11 23:07:37 +0530

    Initial commit

---
+++ src/main/java/com/techprimers/config/configclientsampleexample/ConfigClientSampleExampleApplication.java
@@ -0,0 +1,12 @@
+package com.techprimers.config.configclientsampleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientSampleExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientSampleExampleApplication.class, args);
+    }
+}
